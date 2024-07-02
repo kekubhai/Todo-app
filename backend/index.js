909f3d04a17dd2c2b65f14401d85b029075ddefe
anirban <anirbanghosh060@gmail.com>
2024-07-02 10:16:24 +0530

    Add File

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express= require("express")
+const app= express();
+
+app.post("/todo", function(req,res){ 
+
+})
+
+app.get("/todos", function(req,res){ 
+    
+})
+
+
+app.put("/completed", function(req,res){ 
+    
+})
